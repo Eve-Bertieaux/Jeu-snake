4d29f4a9ca9b9a4bc81e67150f69df081c352a8e
Eve Bertieaux <128031772+Eve-Bertieaux@users.noreply.github.com>
2023-09-29 17:07:41 +0200

    Update main.js

--- jeuduserpent/main.js
+++ jeuduserpent/main.js
@@ -1,4 +1,5 @@
-window.onload = function() {
+window.onload = function() 
+{
     var canvasWidth = 900;
     var canvasHeight = 600;
     var blockSize = 30;
@@ -8,7 +9,8 @@ window.onload = function() {
 
     init();
 
-    function init() {
+    function init() 
+    {
         var canvas = document.createElement('canvas');
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
@@ -21,33 +23,41 @@ window.onload = function() {
     
     
 
-    function refreshCanvas() {
+    function refreshCanvas() 
+    {
         ctx.clearRect(0, 0,canvasWidth, canvasHeight);
         snakee.advance();
         snakee.draw();
         setTimeout(refreshCanvas, delay);
     };
 
-    function drawBlock(ctx, position) {
+    function drawBlock(ctx, position) 
+    {
         var x = position[0] * blockSize;
         var y = position[1] * blockSize;
         ctx.fillRect(x, y, blockSize, blockSize);
     }
 
-    function Snake(body, direction) {
+    function Snake(body, direction) 
+    {
         this.body = body;
         this.direction = direction;
-        this.draw = function() {
+        this.draw = function() 
+        {
             ctx.save();
             ctx.fillStyle = "#FF0000";
-            for (var i = 0; i < this.body.length; i++) {
+            for (var i = 0; i < this.body.length; i++) 
+            {
                 drawBlock(ctx, this.body[i]);
             }
             ctx.restore();
         };
-        this.advance = function() {
+        
+        this.advance = function() 
+        {
             var nextPosition = this.body[0].slice();
-            switch (this.direction) {
+            switch (this.direction) 
+            {
                 case "left":
                     nextPosition[0] -= 1
                     break;
@@ -84,10 +94,12 @@ window.onload = function() {
 
     }
 
-    document.onkeydown = function handleKeyDown(e) {
+    document.onkeydown = function handleKeyDown(e) 
+    {
         var key = e.keyCode;
         var newDirection;
-        switch (key) {
+        switch (key) 
+        {
             case 37:
                 newDirection = "left";
                 break;
